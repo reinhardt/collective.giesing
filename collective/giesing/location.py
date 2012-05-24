711222f9cde0ffbb211705543c393e3321fcabc4
Manuel Reinhardt <Manuel.Reinhardt@neon-cathedral.net>
2012-05-24 16:26:55 +0100

    initial commit

---
+++ collective/giesing/location.py
@@ -0,0 +1,31 @@
+from five import grok
+from zope import schema
+
+from plone.directives import form, dexterity
+
+from plone.app.textfield import RichText
+
+from collective.giesing import GiesingMessageFactory as _
+
+class ILocation(form.Schema)
+    """ A location is a real or fictional place where a snippet can be set. A 
+        guideline for the size of a location is "as far as the eye can see".
+        Examples are a flat, a bar, a forest clearing etc.
+    """
+
+    title = schema.TextLine(
+            title=_(u"Location Name"),
+        )
+
+    description = RichText(
+            title=_(u"Description"),
+            required=False,
+        )
+
+    coordinates = schema.Tuple(
+            title=_("Coordinates"),
+            required=False,
+            min_length=2,
+            max_length=2,
+            value_type=Float(title=_("Coordinate")),
+        )
---
+++ collective/giesing/snippet.py
@@ -0,0 +1,43 @@
+from five import grok
+from zope import schema
+
+from plone.directives import form, dexterity
+
+from plone.app.textfield import RichText
+from z3c.relationfield.schema import RelationChoice
+from plone.formwidget.contenttree import ObjPathSourceBinder
+
+from collective.giesing import GiesingMessageFactory as _
+from collective.giesing.location import ILocation
+
+class ISnippet(form.Schema):
+    """ A snippet is a short (a few paragraphs) prose text, usually part of a storline
+    """
+
+    title = schema.TextLine(
+            title=_(u"Title"),
+        )
+    
+    description = schema.Text(
+            title=_(u"Description"),
+            required=False,
+        )
+
+    text = RichText(
+            title=_(u"Text"),
+            required=False,
+        )
+
+    timetag = schema.Datetime(
+            title=_(u"Timetag"),
+            description=_("The approximate time at which the events that this"
+                          "snippet describes take place"),
+            required=False,
+        )
+
+    locationtag = RelationChoice(
+            title=_(u"Locationtag"),
+            source=ObjPathSourceBinder(object_provides=ILocation.__identifier__),
+            required=False,
+        )
+

2c2f1053cd3b027beeaed165a83f70ce5e0447fa
Manuel Reinhardt <Manuel.Reinhardt@neon-cathedral.net>
2012-07-29 12:35:07 +0000

    added LocationTimeLineViewlet, reordered viewlets

--- collective/giesing/browser/viewlets.py
+++ collective/giesing/browser/viewlets.py
@@ -1,12 +1,18 @@
 from five import grok
+from zope.interface import Interface
+
 from Products.Five.browser.pagetemplatefile import ZopeTwoPageTemplateFile
 from plone.app.layout.viewlets.interfaces import IPortalHeader
-from zope.interface import Interface
 from plone.app.layout.viewlets.interfaces import IBelowContent
+
 from Products.CMFCore.utils import getToolByName
+from zc.relation.interfaces import ICatalog
+from zope.app.intid.interfaces import IIntIds
+from zope.component import getUtility
 
 from collective.giesing.snippet import ISnippet
 from collective.giesing.storyline import IStoryline
+from collective.giesing.location import ILocation
 
 grok.templatedir('templates')
 
@@ -27,3 +33,22 @@ class TimeLineViewlet(grok.Viewlet):
         timeline = past[-3:-1] + future[:3]
         return timeline
 
+class LocatineTimeLineViewlet(grok.Viewlet):
+    grok.context(ILocation)
+    grok.name('collective.giesing.LocationTimeLineViewlet')
+    grok.require('zope2.View')
+    grok.viewletmanager(IBelowContent)
+    grok.template('timelineviewlet')
+
+    def getTimeLine(self):
+        intids = getUtility(IIntIds)
+        relcatalog = getUtility(ICatalog)
+
+        snippets = [intids.getObject(rv.from_id) for rv in relcatalog.findRelations({'to_id': intids.getId(self.context)})]
+        timeline = [dict(Title=obj.Title(), 
+                         getURL=obj.absolute_url(), 
+                         storyline=obj.aq_parent.Title(), 
+                         timetag=obj.timetag) \
+                    for obj in snippets]
+        timeline.sort(key=lambda x: x['timetag'])
+        return timeline

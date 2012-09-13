5cfef906a3e6081bd902dfa4738778e2c88755a4
Manuel Reinhardt <Manuel.Reinhardt@neon-cathedral.net>
2012-09-13 20:00:56 +0100

    added translations

---
+++ collective/giesing/i18n.py
@@ -0,0 +1,10 @@
+from collective.giesing import GiesingMessageFactory as _
+
+additional_i18n_strings = [
+_("Location"),
+_("description_location", "a setting for a snippet"),
+_("Storyline"),
+_("description_storyline", "a set of logically connected snippets"),
+_("Snippet"),
+_("description_snippet", "a short prose text, usually part of a storyline"),
+]

6076609f63799ebd8557a85c6ab5fc20dde9a315
Manuel Reinhardt <Manuel.Reinhardt@neon-cathedral.net>
2013-03-29 01:00:43 +0000

    custom rss view for storyline objects

--- collective/giesing/storyline.py
+++ collective/giesing/storyline.py
@@ -2,9 +2,13 @@ from five import grok
 from zope import schema
 
 from plone.directives import form, dexterity
+from Products.CMFCore.utils import getToolByName
 
 from collective.giesing import GiesingMessageFactory as _
 from collective.giesing.location import ILocation
+from collective.giesing.interfaces import IGiesingLayer
+
+grok.templatedir('templates')
 
 class IStoryline(form.Schema):
     """ A storyline is a set of logically connected snippets """
@@ -18,3 +22,18 @@ class IStoryline(form.Schema):
             required=False,
         )
 
+class RSS(grok.View):
+    """ A storyline specific RSS view """
+    grok.context(IStoryline)
+    grok.layer(IGiesingLayer)
+    grok.template('storyline_rss')
+
+    def update(self):
+        catalog = getToolByName(self.context, 'portal_catalog')
+        self.object_list = catalog(path='/'.join(self.context.getPhysicalPath()),
+                                   portal_type='collective.giesing.snippet',
+                                   sort_on='modified',
+                                   sort_order='reverse',
+                                   b_start=0,
+                                   b_size=10,
+                                   )
